047f1d853546f029cc05bd30d11fdf3b6218046f
Zoufuheng <2671915558@qq.com>
2020-05-27 09:39:13 +0800

    修改志愿者信息文件

---
+++ src/main/java/com/pwpew/dao/VolunteerDao.java
@@ -0,0 +1,21 @@
+package com.pwpew.dao;
+
+import com.pwpew.entity.TUser;
+import com.pwpew.entity.TVolunteer;
+
+import java.util.List;
+
+public interface VolunteerDao {
+    //增删改查
+    //查询所有用户
+    public List<TVolunteer> findVolunteer();
+    public TUser getVolunteer(TVolunteer volunteer);
+    public void updateVolunteer(TVolunteer volunteer);
+    public void deleteVolunteer(TVolunteer volunteer);
+    public void insertVolunteer(TVolunteer volunteer);
+
+    //分页查询用户
+    public List<TVolunteer> findUserByPage(TVolunteer volunteer, int firstResult, int maxResults);
+    //查询用户总数，用于分页
+    public Long findUserCount(TVolunteer volunteer);
+}
